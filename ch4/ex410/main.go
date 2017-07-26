b52b20dba77932ab3ab25cfa1ee89d302c06af8c
cduong13 <chrisduong83@gmail.com>
2017-07-26 16:05:34 +0700

    ex4.10

---
+++ ch4/ex410/main.go
@@ -0,0 +1,50 @@
+// Modify issues to report the results in age categories, say less than a month old, less than a year old, and more than a year old
+// Page: 405
+//+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/chrisduong/gopl.io/ch4/github"
+)
+
+//!+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	fmt.Println("Issues less than a month")
+	for _, item := range result.Items {
+		if time.Since(item.CreatedAt).Hours()/24 <= 30 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+
+	// Remember to exclude month-old issues for prettier
+	fmt.Println("Issues less than a year")
+	for _, item := range result.Items {
+		if time.Since(item.CreatedAt).Hours()/24 > 30 &&
+			time.Since(item.CreatedAt).Hours()/24 <= 365 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+
+	fmt.Println("Issues more than a year")
+	for _, item := range result.Items {
+		if time.Since(item.CreatedAt).Hours()/24 > 365 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+}
+
+//* go run main.go repo:golang/go is:open json decoder
