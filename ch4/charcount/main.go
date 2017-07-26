73ea6b86ae876bdd9358d3a10d2141cd6662cf7a
cduong13 <chrisduong83@gmail.com>
2017-07-26 18:58:48 +0700

    annotation

--- ch4/charcount/main.go
+++ ch4/charcount/main.go
@@ -31,6 +31,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
 			os.Exit(1)
 		}
+		// ReadRune() would convert invalid unicode char to ReplacementChar
 		if r == unicode.ReplacementChar && n == 1 {
 			invalid++
 			continue
