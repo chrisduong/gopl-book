31c89da711a977f5aa6f65f2f2300bd82a8fcaa1
cduong13 <chrisduong83@gmail.com>
2017-07-28 18:06:11 +0700

    done ex49

--- ch4/ex49/main.go
+++ ch4/ex49/main.go
@@ -2,6 +2,25 @@
 
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	// NOTE: Don't know why no parameters is accepted for `bufio.ScanWords`.
+	// MAYBE: input is already a Scanner so it is OK.
+	// Split words in Scanner
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for w, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, w)
+		}
+	}
 }
