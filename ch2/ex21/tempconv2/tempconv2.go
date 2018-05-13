58dd7e87834b47f5d7d396f05ca4dfdd20dc20ae
Chris Duong <chrisduong83@gmail.com>
2018-05-13 17:13:54 +0800

    KToC

--- ch2/ex21/tempconv2/tempconv2.go
+++ ch2/ex21/tempconv2/tempconv2.go
@@ -12,6 +12,7 @@ type Celsius float64
 
 // Fahrenheit type
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	// AbsoluteZeroC of Celsius
@@ -24,3 +25,4 @@ const (
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
