71a8dfce2ed767b51a36be3e53dfd7b92f0ca621
Chris Duong <chrisduong83@gmail.com>
2018-05-13 22:08:02 +0800

    done for input is 30.3m

--- ch2/ex22/main.go
+++ ch2/ex22/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 // XXX: the exercise suggested that the input can say about its Measurement,
@@ -15,6 +18,7 @@ type Feet float64
 
 type Meter float64
 
+// Measurement interface to conditionally print corresponding Unit type
 type Measurement interface {
 	String() string
 }
@@ -26,10 +30,45 @@ type Kilogram float64
 func (ft Feet) String() string {
 	return fmt.Sprintf("%.3gft", ft)
 }
+
 func (m Meter) String() string {
 	return fmt.Sprintf("%.3gm", m)
 }
 
+// makeMeasurement return the specific measurement
+func makeMeasurement(f float64, unit string) (Measurement, error) {
+	unit = strings.ToLower(unit)
+	switch unit {
+	case "m":
+		return Meter(f), nil
+	case "ft":
+		return Feet(f), nil
+	default:
+		return nil, fmt.Errorf("Unexpected unit %v", unit)
+	}
+}
+
+// analyseInput use Regex to capture Value and Unit kind if it is input correctly
+func analyseInput(s string) (float64, string, error) {
+	// Can you https://regex101.com to quickly golang regexp
+	re := regexp.MustCompile(`(^-?\d+(?:\.\d+)?)([A-Za-z]+)`)
+	match := re.FindStringSubmatch(s)
+	if match == nil {
+		log.Fatalf("Expecting <value>.<unit>, got %q", s)
+	}
+	v, err := strconv.ParseFloat(match[1], 64)
+	if err != nil {
+		log.Fatalf("%v isn't a number.", match[1])
+	}
+	if match[2] == "" {
+		log.Fatalf("No unit specified.")
+	}
+	unit := match[2]
+
+	return v, unit, nil
+}
+
+// FeetToMeter converts Feet to Meter
 func FeetToMeter(ft Feet) Meter {
 	return Meter(ft * 0.3)
 }
@@ -38,13 +77,22 @@ func main() {
 
 	if len(os.Args) > 1 {
 		for _, arg := range os.Args[1:] {
-			v, err := strconv.ParseFloat(arg, 64)
+			// v, err := strconv.ParseFloat(arg, 64)
+			// if err != nil {
+			// 	fmt.Printf("An error occurred %q", err)
+			// 	os.Exit(2)
+			// }
+			// fmt.Printf("The Feet is %s;", Feet(v).String())
+			// fmt.Printf("then converted to Meter is %s", FeetToMeter(Feet(v)).String())
+
+			v, unit, err := analyseInput(arg)
 			if err != nil {
-				fmt.Printf("An error occurred %q", err)
+				fmt.Printf("An Input error occurred %q", err)
 				os.Exit(2)
 			}
-			fmt.Printf("The Feet is %s;", Feet(v).String())
-			fmt.Printf("then converted to Meter is %s", FeetToMeter(Feet(v)).String())
+
+			m, _ := makeMeasurement(v, unit)
+			fmt.Printf("You input this measurement %s", m.String())
 		}
 	} else {
 		scan := bufio.NewScanner(os.Stdin)
