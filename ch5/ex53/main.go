ba75ccb15c3c4cefd96db8e56cf3bf582692c3c3
Chris Duong <chrisduong83@gmail.com>
2018-08-05 18:04:51 +0800

    define ex53

---
+++ ch5/ex53/main.go
@@ -0,0 +1,7 @@
+// “Write a function to print the contents of all text nodes in an HTML document tree. Do not descend into <script> or <style> elements, since their contents are not visible in a web browser.”
+
+package main
+
+func main() {
+
+}
