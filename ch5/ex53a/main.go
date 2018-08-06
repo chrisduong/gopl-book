eb3c162b032bf3fb9fcd885a4f72ad2f6a9faf7b
Chris Duong <chrisduong83@gmail.com>
2018-08-06 09:06:53 +0800

    53a

---
+++ ch5/ex53a/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+// https://github.com/nasciiboy/TGPL-Exercises/blob/master/05-03/nasciiboy/main.go
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+
+	pPrint(doc)
+}
+
+// visit appends to links each link found in n and returns the result.
+func pPrint(n *html.Node) {
+	if n.Type == html.ElementNode {
+		if n.Data == "script" || n.Data == "style" {
+			return
+		}
+	}
+
+	if n.Type == html.TextNode {
+		fmt.Printf("%s", n.Data)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		pPrint(c)
+	}
+}
