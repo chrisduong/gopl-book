6ce8532b0577182129e319268298acf57b31d7b0
Chris Duong <chrisduong83@gmail.com>
2018-08-07 11:18:44 +0800

    copy functions from book

--- ch5/ex55/main.go
+++ ch5/ex55/main.go
@@ -2,6 +2,33 @@
 
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
 func main() {
 
 }
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int)
