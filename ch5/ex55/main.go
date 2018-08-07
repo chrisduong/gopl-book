ea74531571e58940430eaac36052f34cc2286cb0
Chris Duong <chrisduong83@gmail.com>
2018-08-07 14:33:44 +0800

    TODO: the words supposed to be accummulated during recursion, but it won't

--- ch5/ex55/main.go
+++ ch5/ex55/main.go
@@ -3,14 +3,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "CountWordsAndImages: %v\n", err)
+			continue
+		}
+		fmt.Printf("URL %s has %d words and %d images", url, words, images)
+	}
 }
 
 // CountWordsAndImages does an HTTP GET request for the HTML
@@ -28,7 +38,43 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 		return
 	}
 	words, images = countWordsAndImages(doc)
+	fmt.Println(words)
 	return
 }
 
-func countWordsAndImages(n *html.Node) (words, images int)
+func countWordsAndImages(n *html.Node) (words, images int) {
+	// TODO: the words supposed to be accummulated during recursion, but it won't
+	if n.Type == html.TextNode {
+		if strings.TrimSpace(n.Data) != "" {
+			words += wordCount(n.Data)
+		}
+	}
+
+	if n.Type == html.ElementNode {
+		if n.Data == "img" {
+			for _, i := range n.Attr {
+				if i.Key == "src" {
+					images++
+				}
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		countWordsAndImages(c)
+	}
+
+	return
+}
+
+func wordCount(s string) int {
+	var count int
+	input := bufio.NewScanner(strings.NewReader(s))
+	input.Split(bufio.ScanWords)
+
+	for input.Scan() {
+		count++
+	}
+
+	return count
+}
