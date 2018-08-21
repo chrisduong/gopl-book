c252aee71a49c237a8aedabca03bc4655f688cfa
Chris Duong <chrisduong83@gmail.com>
2018-08-21 22:45:49 +0800

    annotate pretty_test

--- ch5/ex57/pretty_test.go
+++ ch5/ex57/pretty_test.go
@@ -22,12 +22,15 @@ func TestPrettyOutputCanBeParsed(t *testing.T) {
 		t.Error(err)
 	}
 	pp := NewPrettyPrinter()
+	// You need to initialize a pointer because the method `Write` is on the pointer of `Buffer` type
 	b := &bytes.Buffer{}
 	err = pp.Pretty(b, doc)
+	// Check if the Pretty method failed
 	if err != nil {
 		t.Log(err)
 		t.Fail()
 	}
+	// Check if the output of the method can be parse
 	_, err = html.Parse(bytes.NewReader(b.Bytes()))
 	if err != nil {
 		t.Log(err)
