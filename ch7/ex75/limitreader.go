707ff4e2056a1b1d38a74fe07687874104c56ac6
Chris Duong <chrisduong83@gmail.com>
2018-12-19 22:25:18 +0800

    ex75

---
+++ ch7/ex75/limitreader.go
@@ -0,0 +1,38 @@
+// The LimitReader function in the io package accepts an io.Reader r and a number of bytes n, and returns another Reader that reads from r but reports an end-of-file condition after n bytes. Implement it
+// “func LimitReader(r io.Reader, n int64) io.Reader”
+
+package limitreader
+
+import (
+	"fmt"
+	"io"
+)
+
+type IOLimitReader struct {
+	R io.Reader
+	N int64
+}
+
+// A LimitedReader reads from R but limits the amount of
+// data returned to just N bytes. Each call to Read
+// updates N to reflect the new amount remaining.
+// Read returns EOF when N <= 0 or when the underlying R returns EOF.
+func (l *IOLimitReader) Read(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, io.EOF
+	}
+	if l.N < int64(len(p)) {
+		p = p[0:l.N]
+	}
+	n, err = l.R.Read(p)
+	l.N -= int64(n)
+	fmt.Println(p)
+	return
+}
+
+// LimitReader returns a Reader that reads from r
+// but stops with EOF after n bytes.
+// The underlying implementation is a *LimitedReader.
+func LimitReader(r io.Reader, n int64) *IOLimitReader {
+	return &IOLimitReader{r, n}
+}
---
+++ ch7/ex75/limitreader_test.go
@@ -0,0 +1,23 @@
+package limitreader
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestLimitReader(t *testing.T) {
+	s := "hi there"
+	b := make([]byte, 1024)
+	r := LimitReader(strings.NewReader(s), 4)
+	b = b[:4]
+	_, _ = r.Read(b)
+	// need to read again to return EOF
+	n, err := r.Read(b)
+	if err != io.EOF {
+		t.Log(n)
+		t.Log(err)
+		t.Fail()
+	}
+
+}
