7a17228ce1ccb8bbf9928cedfaf5252b097077a6
Chris Duong <chrisduong83@gmail.com>
2019-01-09 15:36:38 +0800

    update gitignore

---
+++ ch7/ex717/main.go
@@ -0,0 +1,7 @@
+// “Extend xmlselect so that elements may be selected not just by name, but by their attributes too, in the manner of CSS, so that, for instance, an element like <div id="page" class="wide"> could be selected by a matching id or class as well as its name”
+
+package main
+
+func main() {
+	
+}
