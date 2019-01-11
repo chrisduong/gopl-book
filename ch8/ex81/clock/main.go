5f91a8e40cc19edae893386097f69814a0a4913c
Chris Duong <chrisduong83@gmail.com>
2019-01-11 10:12:13 +0800

    refactor ex81

---
+++ ch8/ex81/clock/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	port := flag.Int("port", 8080, "listen port")
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//!+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn) // handle connections concurrently
+	}
+	//!-
+}
+
+// *!*
+// TZ=US/Eastern    bin/clock2 -port 8010 &
+// TZ=Asia/Tokyo    bin/clock2 -port 8020 &
+// TZ=Europe/London bin/clock2 -port 8030 &
--- ch8/ex81/clockwall/main.go
+++ ch8/ex81/clockwall/main.go
@@ -1,3 +1,11 @@
+// “Modify clock2 to accept a port number, and write a program, clockwall, that acts as a client of several clock servers at once, reading the times from each one and displaying the results in a table, akin to the wall of clocks seen in some business offices. If you have access to geographically distributed computers, run instances remotely; otherwise run local instances on different ports with fake time zones”
+//
+// “$ TZ=US/Eastern    ./clock2 -port 8010 &
+// $ TZ=Asia/Tokyo    ./clock2 -port 8020 &
+// $ TZ=Europe/London ./clock2 -port 8030 &
+// $ bin/clockwall NewYork=localhost:8010 London=localhost:8020 Tokyo=localhost:8030
+
+// - Page 461
 package main
 
 import (
@@ -55,3 +63,16 @@ func main() {
 		time.Sleep(time.Minute)
 	}
 }
+
+// *-*
+// > bin/clockwall NewYork=localhost:8010 London=localhost:8020 Tokyo=localhost:80
+// 30
+// London: 11:08:11
+// NewYork: 21:08:11
+// Tokyo: 02:08:11
+// NewYork: 21:08:12
+// London: 11:08:12
+// Tokyo: 02:08:12
+// Tokyo: 02:08:13
+// London: 11:08:13
+// NewYork: 21:08:1
