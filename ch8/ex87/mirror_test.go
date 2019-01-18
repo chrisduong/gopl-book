c29a3ce47eaf25017289db5dae8b42ea3348a9d7
Chris Duong <chrisduong83@gmail.com>
2019-01-18 10:36:47 +0800

    added mirror_test

---
+++ ch8/ex87/mirror_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/url"
+	"strings"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func TestRewriteLocalLinks(t *testing.T) {
+	url_, err := url.Parse("http://www.rah.com/a/b/c")
+	if err != nil {
+		t.Fatal(err)
+	}
+	base = url_
+	input := `<html><body><a href="http://www.rah.com/d/e">text</a></body></html>`
+	doc, err := html.Parse(strings.NewReader(input))
+	if err != nil {
+		t.Fatal(err)
+	}
+	nodes := linkNodes(doc)
+	
+	(nodes, url_)
+	want := "/d/e"
+	got := nodes[0].Attr[0].Val
+	if got != want {
+		t.Fatalf("got %q, want %q", got, want)
+	}
+}
