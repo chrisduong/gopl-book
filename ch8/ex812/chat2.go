e5c53173868da1b85a2fd6e3749d3c58b697ab10
Chris Duong <chrisduong83@gmail.com>
2019-01-20 11:57:58 +0800

    state ex812

---
+++ ch8/ex812/chat2.go
@@ -0,0 +1,7 @@
+// “Make the broadcaster announce the current set of clients to each new arrival. This requires that the clients set and the entering and leaving channels record the client name too”
+
+package main
+
+func main() {
+
+}
